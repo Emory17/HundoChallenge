e98d16d90f714a1773be8a25ecdedcdb7d1ec117
Emory Soper <emorysoper@icloud.com>
2023-04-11 16:24:07 -0400

    developed

--- js/site.js
+++ js/site.js
@@ -1,11 +1,28 @@
-function displayMessage() {
-    let msg = document.getElementById("message").value;
-    //alert(msg);
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg
+function displayResults() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    let table = document.getElementById("results");
+
+    startNumber  = parseInt(startValue);
+    endNumber = parseInt(endValue);
+
+    table.innerHTML = "";
+
+    if (startNumber <= endNumber) {
+        for (i = startValue; i <= endValue; i++) {
+            if (i % 2 == 0) {
+                table.innerHTML += "<tr><td><strong>" + i + "</strong></tr></td>";
+            } else {
+                table.innerHTML += "<tr><td>" + i + "</tr></td>";
+            }
         }
-    );
+    } else {
+        for (i = startValue; i >= endValue; i--) {
+            if (i % 2 == 0) {
+                table.innerHTML += "<tr><td><strong>" + i + "</strong></tr></td>";
+            } else {
+                table.innerHTML += "<tr><td>" + i + "</tr></td>";
+            }
+        }
+    }
 }
